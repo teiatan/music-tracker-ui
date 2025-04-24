1d3810dcf9f7f1608da103410d73fddc0b103ec9
ProtasTetiana <92535482+teiatan@users.noreply.github.com>
2025-04-25 02:38:41 +0300

    feat: add loader message

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,6 +4,7 @@ const Loader = () => {
   return (
     <div className={styles.overlay} data-testid="loading-tracks">
       <div className={styles.spinner}></div>
+      <p className={styles.message}>Loading can take up to 3 minutes</p>
     </div>
   );
 };
